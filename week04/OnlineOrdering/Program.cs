2c46247a6423893b6ac5cf28f76253f09f78ecae
ninafuentes <nfuentesguerrero@byupathway.edu>
2026-08-01 17:13:18 -0500

    week 4 o

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        
+        Address address1 = new Address(
+            "123 Main Street",
+            "New York",
+            "NY",
+            "USA");
+
+        Customer customer1 = new Customer("John Smith", address1);
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Laptop", "P100", 800, 1));
+        order1.AddProduct(new Product("Mouse", "P101", 25, 2));
+        order1.AddProduct(new Product("Keyboard", "P102", 40, 1));
+
+    
+        Address address2 = new Address(
+            "45 Central Ave",
+            "Toronto",
+            "Ontario",
+            "Canada");
+
+        Customer customer2 = new Customer("Emily Brown", address2);
+
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Phone", "P200", 600, 1));
+        order2.AddProduct(new Product("Headphones", "P201", 80, 2));
+
+        
+        Console.WriteLine("===== ORDER 1 =====");
+
+        Console.WriteLine("\nPacking Label:");
+        Console.WriteLine(order1.GetPackingLabel());
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.GetShippingLabel());
+
+        Console.WriteLine($"\nTotal Price: ${order1.GetTotalCost()}");
+
+       
+        Console.WriteLine("\n============================");
+        Console.WriteLine("===== ORDER 2 =====");
+
+        Console.WriteLine("\nPacking Label:");
+        Console.WriteLine(order2.GetPackingLabel());
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.GetShippingLabel());
+
+        Console.WriteLine($"\nTotal Price: ${order2.GetTotalCost()}");
     }
 }
