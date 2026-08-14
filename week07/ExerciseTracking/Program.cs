00abc22871fbfaa4153a3119c736bb3249b8e5ea
ninafuentes <nfuentesguerrero@byupathway.edu>
2026-08-13 19:33:11 -0500

    w7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(
+            new DateTime(2022, 11, 3),
+            30,
+            4.8
+        );
+
+        Cycling cycling = new Cycling(
+            new DateTime(2022, 11, 3),
+            30,
+            15.0
+        );
+
+        Swimming swimming = new Swimming(
+            new DateTime(2022, 11, 3),
+            30,
+            40
+        );
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
